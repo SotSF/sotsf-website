22bae093536b6c2f021f3b8270f11c4e54d06cd4
Kevin Reynolds <tossrock@gmail.com>
2024-11-09 21:38:01 -0800

    Move under construction page to Doors of Durin, remove copyright footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,26 +7,18 @@ import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
 
 import Logo from '~/svg/Logo.svg';
+import gate from 'public/images/gate.jpg';
+import Image from 'next/image';
 
 export default function HomePage() {
   return (
     <main>
-      <section className='bg-dark'>
+      <section className='bg-black'>
         <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo className='w-16' />
-          <h1 className='mt-4'>Servants of the Secret Fire</h1>
-          <p className='mt-2 text-sm'>you shall not pass, etc.</p>
-
-          <ButtonLink variant='outline' className='my-10' href='/canopy'>
-            Canopy page
-          </ButtonLink>
-
-          <Hammer size='6em' />
-          <p className='mt-2'>Currently under construction!</p>
-
-          <UnderlineLink className='mt-2' href='/components'>
-            Component library
-          </UnderlineLink>
+          <Image
+            src={gate}
+            alt="Speak friend and enter"
+          ></Image>
         </div>
       </section>
     </main>
