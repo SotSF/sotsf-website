d783a1f8dd6133e53d9032085c2f623648872767
Ben Rollin <ben.rollin@clever.com>
2024-12-08 13:52:45 -0800

    install shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
-import clsx, { ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-/** Merge classes with tailwind-merge with clsx full feature */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
